bcee1dc1e1075adde8fb260f31865af5f4952f25
samueljames <samuel4abiodun@gmail.com>
2019-03-03 23:52:12 +0100

    Fix s3 content-type error

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ export BUCKET="blog.vtion.ai"
 export DEPLOY_DIR=".deploy"
 # Copy the site directory to a temporary location so that modifications we make don't get overwritten by the Jekyll server
 # that is potentially running
-mkdir -p $DEPLOY_DIR
+mkdir -p $DEPLOY_DIR $DEPLOY_DIR/content
 
 cp -a ../jasper2-pages/. $DEPLOY_DIR
 
@@ -20,13 +20,13 @@ for filename in $DEPLOY_DIR/*.html; do
         filename="${filename%.*}"
 
         # Move it
-        mv $original $DEPLOY_DIR/$filename
+        mv $original $DEPLOY_DIR/content/$filename
     fi
 done
 
 # Now upload to s3, deleting any items that no longer exist
-aws s3 sync --delete $DEPLOY_DIR s3://$BUCKET
+aws s3 sync --delete $DEPLOY_DIR s3://$BUCKET --exclude $DEPLOY_DIR/content/*
 # Finally, upload the blog directory specifically to force the content-type
-aws s3 cp "$DEPLOY_DIR" s3://$BUCKET #--recursive --content-type "text/html"
+aws s3 cp "$DEPLOY_DIR/content" s3://$BUCKET --recursive --content-type "text/html"
 # Cleanup
 rm -r $DEPLOY_DIR
